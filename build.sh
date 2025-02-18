6f94d0d351d2ae34ab02de9be6982626fd031cb9
captivator <84224501+qaptivator@users.noreply.github.com>
2025-02-18 13:29:26 +0200

    oh wait so i dont need spaces in between?

--- build.sh
+++ build.sh
@@ -21,8 +21,12 @@ args=()
 ! $yesconsole && args+=("-mwindows")
 $static && args+=("-static" "-l winmm" "-l ole32" "-l setupapi" "-l imm32" "-l version" "-l oleaut32" "-l uuid" "-l mfplat")
 
+
+# "-l winmm" "-l ole32" "-l setupapi" "-l imm32" "-l version" "-l oleaut32" "-l uuid" "-l mfplat"
+# -l winmm -l ole32 -l setupapi -l imm32 -l version -l oleaut32 -l uuid -l mfplat
+
 if [ -f "./src/plainIMG_rc.o" ]; then
-  gcc "${args[@]}" -o "${appName}.exe" src/plainIMG_rc.o  src/main.c -I ./src/SDL3/include -I ./src/SDL3_Image/include -L ./src/SDL3/lib -L ./src/SDL3_Image/lib -l SDL3 -l SDL3_image -l Comdlg32
+  gcc "${args[@]}" -o "${appName}.exe" src/plainIMG_rc.o  src/main.c -I ./src/SDL3/include -I ./src/SDL3_Image/include -L ./src/SDL3/lib -L ./src/SDL3_Image/lib -l SDL3 -l SDL3_image -l Comdlg32 -l winmm -l ole32 -l setupapi -l imm32 -l version -l oleaut32 -l uuid -l mfplat
 else
   gcc "${args[@]}" -o "${appName}.exe" src/main.c -I ./src/SDL3/include -I ./src/SDL3_Image/include -L ./src/SDL3/lib -L ./src/SDL3_Image/lib -l SDL3 -l SDL3_image -l Comdlg32
 fi
